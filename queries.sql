926e1e1ede62007a31e8feb5868d83b1501e9488
Akshay <93238948+Ak7349@users.noreply.github.com>
2024-07-24 17:01:13 +0530

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,15 @@
+--Note: below queries are in PostgreSQL modify your query based on your database--
+
+--Create these tables--
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+name VARCHAR(15) UNIQUE NOT NULL,
+color VARCHAR(15)
+);
+
+CREATE TABLE visited_countries(
+id SERIAL PRIMARY KEY,
+country_code CHAR(2) NOT NULL,
+user_id INTEGER REFERENCES users(id)
+);
